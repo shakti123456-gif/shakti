39d04b2231894dfb3cc1870a317448b43942278c
shakti123456-gif <shakti.johnnette@gmail.com>
2020-10-29 10:36:44 +0530

    commit

--- Jzata/settings.py
+++ Jzata/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CompanyData'
 ]
 
 MIDDLEWARE = [
@@ -107,8 +108,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
